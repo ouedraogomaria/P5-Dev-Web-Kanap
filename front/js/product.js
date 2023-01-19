4c3dacfb58d067a454cf808bdfaf82f2b1a696f0
ouedraogomaria <116485128+ouedraogomaria@users.noreply.github.com>
2023-01-19 10:01:54 +0100

    add searchParams

---
+++ front/js/product.js
@@ -0,0 +1,7 @@
+const queryString_url_id = window.location.search;
+const urlParams = new URLSearchParams(queryString_url_id);
+const id = urlParams.get("id");
+
+
+
+    
--- front/js/script.js
+++ front/js/script.js
@@ -1,17 +1,17 @@
 fetch("http://localhost:3000/api/products")
     .then((response) => response.json())
     .then((data) => {
-        for(let article of data){
-            document.querySelector('#items').innerHTML += `     
-            <a href="./product.html?id=42${article._id}">
-            <article>
-              <img src="${article.imageUrl}" alt="Lorem ipsum dolor sit amet, Kanap name1${article.altTxt}">
-              <h3 class="productName">Kanap name1${article.name}</h3>
-              <p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.${article.description}</p>
-            </article>
-          </a> 
-          `
-        }
+      for(let article of data){
+        document.querySelector('#items').innerHTML += `     
+        <a href="./product.html?id=42${article._id}">
+        <article>
+          <img src="${article.imageUrl}" alt="Lorem ipsum dolor sit amet, Kanap name1${article.altTxt}">
+          <h3 class="productName">Kanap name1${article.name}</h3>
+          <p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.${article.description}</p>
+        </article>
+      </a> 
+      `
+    } 
 
 
      
