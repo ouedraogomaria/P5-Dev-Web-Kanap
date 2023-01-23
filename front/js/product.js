7e3152092b75b6772131e07e80daf4131fcc3f09
ouedraogomaria <116485128+ouedraogomaria@users.noreply.github.com>
2023-01-23 12:42:59 +0100

    add colors and price

--- front/js/product.js
+++ front/js/product.js
@@ -14,30 +14,38 @@ fetch(`http://localhost:3000/api/products/${id}`)
 
 
   function afficherProduit (kanap){
-  afficherImage(kanap.imageUrl, kanap.altTxt)
-  afficherTitre(kanap.name)
-  afficherDescription(kanap.description)
- /* afficherCouleurrs(kanap.colors)
-  afficherPrix(kanap.price)*/
+    afficherImage(kanap.imageUrl, kanap.altTxt)
+    afficherTitre(kanap.name)
+    afficherDescription(kanap.description)
+    afficherCouleurs(kanap.colors)
+    afficherPrix(kanap.price)
   console.log('la fonction afficher produits s execute');
   
 }
 function afficherTitre(name){
-  document.querySelector('#title').innerHTML += `     
+  document.querySelector('#title').innerHTML = `     
         <h1>${name}</h1>
         
       `
 }
 function afficherImage(imageUrl, altTxt){
-  document.querySelector('.item__img').innerHTML += `
+  document.querySelector('.item__img').innerHTML = `
   <img src="${imageUrl}" alt="${altTxt}">
   `
 }
 function afficherDescription(description){
-  document.querySelector('#description').innerHTML +=`
+  document.querySelector('#description').innerHTML =`
 <p>${description}</p>`
 }
-
+function afficherCouleurs(colors){
+  document.querySelector('#colors').innerHTML +=`
+    <option value="${colors.vert}">vert</option>
+    <option value="${colors.blanc}">blanc</option> `
+}
+function afficherPrix(Price){
+  document.querySelector('#price').innerHTML =`
+   <span>${Price}</span> `
+}
   
  
 
