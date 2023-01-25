9ee78c2f014d23e8b01d8d467deb811cd31039ca
ouedraogomaria <116485128+ouedraogomaria@users.noreply.github.com>
2023-01-25 20:50:35 +0100

    add localstorage

--- front/js/product.js
+++ front/js/product.js
@@ -1,27 +1,28 @@
-// Recuperer le id et afficher une image avec son contenu
+// Recuperer l' id d'un produit à afficher
 const queryString_url_id = window.location.search;
 const urlParams = new URLSearchParams(queryString_url_id);
 const id = urlParams.get("id");
-console.log(id);
 
+// Recuper les donnees des produits depuis l API
 fetch(`http://localhost:3000/api/products/${id}`)
   .then((response) => response.json())
   .then((data) =>{
        afficherProduit (data)
-       console.log(data)
+
        
   } )
 
-
-  function afficherProduit (kanap){
-    afficherImage(kanap.imageUrl, kanap.altTxt)
-    afficherTitre(kanap.name)
-    afficherDescription(kanap.description)
-    afficherCouleurs(kanap.colors)
-    afficherPrix(kanap.price)
-  console.log('la fonction afficher produits s execute');
+  
+function afficherProduit (kanap){
+  afficherImage(kanap.imageUrl, kanap.altTxt)
+  afficherTitre(kanap.name)
+  afficherDescription(kanap.description)
+  afficherCouleurs(kanap.colors)
+  afficherPrix(kanap.price)
   
 }
+
+// Inserer l'image d'un produit et ses details 
 function afficherTitre(name){
   document.querySelector('#title').innerHTML = `     
         <h1>${name}</h1>
@@ -29,24 +30,63 @@ function afficherTitre(name){
       `
 }
 function afficherImage(imageUrl, altTxt){
-  document.querySelector('.item__img').innerHTML = `
+  document.querySelector('.item__img').innerHTML += `
   <img src="${imageUrl}" alt="${altTxt}">
   `
 }
+
 function afficherDescription(description){
-  document.querySelector('#description').innerHTML =`
+  document.querySelector('#description').innerHTML +=`
 <p>${description}</p>`
 }
+
 function afficherCouleurs(colors){
-  document.querySelector('#colors').innerHTML +=`
-    <option value="${colors.vert}">vert</option>
-    <option value="${colors.blanc}">blanc</option> `
+  const select = document.querySelector('#colors')
+  if (select != null) {
+    colors.forEach( (color) => {
+      const option = document.createElement('option')
+      option.value = color
+      option.textContent = color
+      select.appendChild(option)
+    })
+  }
+  
 }
+
 function afficherPrix(Price){
-  document.querySelector('#price').innerHTML =`
+  document.querySelector('#price').innerHTML +=`
    <span>${Price}</span> `
 }
+
+// Ajoutrer des produits dans le panier
+
+document.querySelector('#addToCart').addEventListener('click', () => {
+  // Récupérer  la couleur et de la quantité sélectionnées 
+  const color = document.querySelector('#colors').value;
+  const quantity = document.querySelector('#quantity').value;
+
+  if (color === '') {
+    alert('Choisir une couleur');
+    return;
+  }
   
+  if (quantity < 1 || quantity > 100) {
+    alert('choisir une quantité valide');
+    return;
+  }
+
+  const addTocart = {
+    id: id,
+    color:  color,
+    quantity: quantity,
+  }
+
+  localStorage.setItem(id, JSON.stringify(addTocart))
+
+});
+
+
+
  
 
 
